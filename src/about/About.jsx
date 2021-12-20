3f62a59048d52e46b067390df050fe4898ca1e0e
LeandroVeiga9 <leandro.veiga.clear@gmail.com>
2021-12-20 14:54:56 -0300

    pageHeader component and creating routes

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import PageHeader from "../template/PageHeader";
 
 export default props => (
 
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name="Sobre" small="Nós" />
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus voluptate ut tenetur exercitationem, cum sed sit vitae temporibus nihil inventore quod voluptatem consequatur magni amet, sunt doloremque laborum deserunt voluptates.</p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, fuga similique magni sequi praesentium enim rerum inventore consequuntur! Necessitatibus consequatur sed laboriosam porro ipsam ipsa qui quia voluptate aperiam ab?</p>
+        <h2>Imprensa</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore neque beatae sint omnis tempora, iusto odit ipsam quidem culpa debitis facilis nihil esse quo aliquid ad numquam autem ducimus enim?</p>
     </div>
 
 )

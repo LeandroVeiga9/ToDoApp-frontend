49a757e1ee89a61fde35ab613f561f12623a8999
LeandroVeiga9 <leandro.veiga.clear@gmail.com>
2021-12-20 15:42:10 -0300

    add props

--- src/todo/TodoForm.jsx
+++ src/todo/TodoForm.jsx
@@ -6,10 +6,14 @@ import IconButton from "../template/IconButton";
 export default props => (
     <div role='form' className="todoForm">
         <Grid cols='12 9 10'>
-            <input id="description" className="form-control" placeholder="Adicione uma tarefa" />
+            <input id="description" className="form-control" 
+                placeholder="Adicione uma tarefa" 
+                onChange={props.handleChange}
+                value={props.description} />
         </Grid>
         <Grid cols='12 3 2'> 
-            <IconButton style='primary' icon='plus'></IconButton> 
+            <IconButton style='primary' icon='plus'
+                onClick={props.handleAdd} ></IconButton> 
         </Grid>
     </div>
 )

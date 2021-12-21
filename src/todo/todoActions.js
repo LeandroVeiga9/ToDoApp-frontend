26eb59f0ea21cbfff60c75f976fdf856e1978cae
LeandroVeiga9 <leandro.veiga.clear@gmail.com>
2021-12-21 17:29:36 -0300

    create clear action

--- src/todo/todoActions.js
+++ src/todo/todoActions.js
@@ -18,7 +18,7 @@ export const search = () => {
 export const add = (description) => {
     return dispatch => {
         Axios.post(URL, {description})
-            .then(resp => dispatch({ type: 'TODO_ADDED', payload: resp.data }))
+            .then(resp => dispatch(clear()))
             .then(resp => dispatch(search()))
     }
 }
@@ -42,4 +42,8 @@ export const remove = (todo) => {
         Axios.delete(`${URL}/${todo._id}`)
             .then(resp => dispatch(search()))
     }
+}
+
+export const clear = () => {
+    return { type: 'TODO_CLEAR' }
 }

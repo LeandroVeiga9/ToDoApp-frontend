7f59e119a51d006587a29837cdb8b51564150902
LeandroVeiga9 <leandro.veiga.clear@gmail.com>
2021-12-21 15:40:20 -0300

    integrating with redux

--- src/todo/TodoForm.jsx
+++ src/todo/TodoForm.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
 import Grid from "../template/Grid";
 import IconButton from "../template/IconButton";
 
-export default props => {
+import { changeDescription } from "./todoActions";
+
+
+const TodoForm = props => {
 
     const keyHandler = (e) => {
         if (e.key === 'Enter') {
@@ -19,7 +24,7 @@ export default props => {
             <Grid cols='12 9 10'>
                 <input id="description" className="form-control" 
                     placeholder="Adicione uma tarefa" 
-                    onChange={props.handleChange}
+                    onChange={props.changeDescription}
                     onKeyUp={keyHandler}
                     value={props.description} />
             </Grid>
@@ -35,3 +40,8 @@ export default props => {
     )
 
 }
+
+const mapStateToProps = state => ({description: state.todo.description})
+const mapDispatchToProps = dispatch => bindActionCreators({changeDescription}, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
